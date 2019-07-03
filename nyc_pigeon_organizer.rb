585e137d3ea8d8c59ef98541b8323e495516c561
Marco Frisina <marco@frisina.xyz>
2019-07-03 17:56:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,32 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  new_data = {} #declare empty hashes
-
-  data.each do |attribute_category, data_hash|
-    values.each do |value, array|
+  new_hash = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
       array.each do |name|
-        if hash[name] == nil
-          hash[name] = {}
-          hash[name][attribute_category] = []
-        else
-          hash[name][attribute_category] = []
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+
+        if !new_hash[name].has_key?(property)
+          new_hash[name][property] = []
+        end
+
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property] << attribute.to_s
         end
       end
     end
   end
-
-  hash.each do |name, values|
-    values.each do |hashvalue, array|
-      data.each do |attribute_category, values|
-        values.each do |value, array|
-
-          array.each do |element|
-            if element == name &&hashvalue == attribute_category
-              hash[name][hashvalue] << value.to_s
-            end
-          end
-        end
-      end
-    end
-  end
-hash
+  new_hash
+end
